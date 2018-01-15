994cc46ec4b2d0c0cf1a677bed94c6ff156c3c1d
prashankjauhari1 <prashank.jauhari@oodlestechnologies.com>
2018-01-15 19:45:48 +0530

    added single link list Problem name Remove friends

--- HackerEarth/src/com/hackerearth/problems/SLL.java
+++ HackerEarth/src/com/hackerearth/problems/SLL.java
@@ -1,6 +1,10 @@
 
 //comparison between iterative solutions and recursive solutions
+//Christie and friend removal
 package com.hackerearth.problems;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 //this should be inner class of SLL 
 //but for the time being it is separate class
@@ -21,10 +25,14 @@ class node{
 	public void setNext(node next) {
 		this.next = next;
 	}
+	public String toString(){
+		return ""+this.value;
+	}
 
 }
 public class SLL {
 	node head;
+	node tail;
 	int size=0;
 
 	public node getHead() {
@@ -43,17 +51,27 @@ public class SLL {
 		if(this.head==null){
 			head=n;
 			head.setNext(null);
+			tail=head;
 		}
 		else{
-			node temp=this.head;
-			while(temp.getNext()!=null){
-				temp=temp.getNext();
-			}
-			temp.setNext(n);
+			tail.next=n;
+			tail=n;
 		}
 		size++;
 	}
 
+	public node getTail() {
+		return tail;
+	}
+	public void setTail(node tail) {
+		this.tail = tail;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
 	int length(){
 		return this.size;
 	}
@@ -61,9 +79,10 @@ public class SLL {
 	void printSLL(){
 		node temp=this.head;
 		while(temp!=null){
-			System.out.println(temp.getValue());
+			System.out.print(temp.getValue()+" ");
 			temp=temp.getNext();
 		}
+		System.out.println();
 	}
 
 	//iterative reverse
@@ -79,20 +98,75 @@ public class SLL {
 			current=next;
 			next=temp;
 		}
+		tail=head;
 		head=current;
 	}
+	/*
+	 *  DeleteFriend=false
+    for i = 1 to Friend.length-1
+         if (Friend[i].popularity < Friend[i+1].popularity)
+            delete i th friend
+            DeleteFriend=true
+            break
+    if(DeleteFriend == false)
+        delete the last friend
+	 */
+	void deleteFriend(){
+		if(head==null)return;
+		if(head==tail){
+//			System.out.println("inside this head==tail");
+			head=tail=null;
+			return;
+		}
+		node del=null;
+		node prev=null;
+	for(node temp=head;temp.next!=null;){
+		if(temp.getValue() < temp.getNext().getValue()){
+//			System.out.println("inside temp.getValue() < temp.getNext().getValue");
+//			System.out.println("del"+temp.getValue());
+			del=temp;
+			break;
+		}
+		prev=temp;
+		temp=temp.next;
+	}
+	
+	if(del==head){
+//		System.out.println("inside this del==head");
+		head=head.next;
+		return;
+	}
+	else if(del==tail || del==null){
+	//	    System.out.println("inside this del==tail");
+			prev.next=null;
+			tail=prev;
+			return;
+		}
+	else if(del!=null){
+//		System.out.println("inside del!=null");
+//		System.out.println("prev val"+prev.getValue());
+//		System.out.println("del val"+del.getValue());
+		prev.next=del.next;
+	}
+		
+	
+	}
+	
+	
+	
 
-	void reverseR(){
+	/*void reverseR(){
 		if(head==null)return;
 		node current=head;
 		node next=head.getNext();
 		current.setNext(null);
 		recursiveReverse(next, current);
-	}
+	}*/
 
 	//tail recursive solution
-	void recursiveReverse(node next,node current){
+/*	void recursiveReverse(node next,node current){
 		if(next==null){
+			tail=head;
 			head=current;
 			return;
 		}
@@ -101,27 +175,30 @@ public class SLL {
 		current=next;
 		next=temp;
 		recursiveReverse(next,current);
-	}
+	}*/
 
-	public static void main(String args[]){
-		SLL sll=new SLL();
-
-		for(int i=1;i<=200000;i++)
-			sll.add(i);
-
-		long start=System.currentTimeMillis();
-		//		sll.reverseR();
-		long end=System.currentTimeMillis();
-		//		System.out.println("recursing reverse took"+(end-start));
-
-		start=System.currentTimeMillis();
-		sll.reverse();
-		end=System.currentTimeMillis();
-		System.out.println("iterative reverse took"+(end-start));
-
+public static void main(String args[]){
+		
+		try{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+			SLL sll=new SLL();
+		String nk[]=br.readLine().split(" ");
+		int n=Integer.parseInt(nk[0]);
+		int k=Integer.parseInt(nk[1]);
+		String input[]=br.readLine().split(" ");
+		for(int i=0;i<n;i++){
+			sll.add(Integer.parseInt(input[i]));
+		}
+		for(int i=0;i<k;i++){
+			sll.deleteFriend();
+		}
 		sll.printSLL();
-
-		System.out.println("size of sll :"+sll.length());
-
+		sll=null;
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
