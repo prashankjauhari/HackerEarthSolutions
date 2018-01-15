723fe0dad3173d4c14694995909e329d24da25f8
prashankjauhari1 <prashank.jauhari@oodlestechnologies.com>
2018-01-15 23:54:12 +0530

    added chrisine and remove friend code

--- HackerEarth/src/com/hackerearth/problems/SLL.java
+++ HackerEarth/src/com/hackerearth/problems/SLL.java
@@ -4,6 +4,7 @@
 package com.hackerearth.problems;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
 //this should be inner class of SLL 
@@ -11,14 +12,30 @@ import java.io.InputStreamReader;
 class node{
 	private int value;
 	node next;
+	node prev;
+	public node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(node prev) {
+		this.prev = prev;
+	}
+
 	public int getValue() {
 		return value;
 	}
 	
+	node(){};
+	node(int n){
+		this.value=n;
+		this.next=null;
+		this.prev=null;
+	}
+
 	public void setValue(int value) {
 		this.value = value;
 	}
-	
+
 	public node getNext() {
 		return next;
 	}
@@ -41,9 +58,7 @@ public class SLL {
 	public void setHead(node head) {
 		this.head = head;
 	}
-	//add is consuming O(size) time, this can be reduced if we maintain tail pointer
-	//then time can be reduced to O(1)i.e reference manipulation
-	//code needed to be modified for that
+
 	void add(int value){
 		node n=new node();
 		n.setValue(value);
@@ -51,9 +66,11 @@ public class SLL {
 		if(this.head==null){
 			head=n;
 			head.setNext(null);
+			head.setPrev(null);
 			tail=head;
 		}
 		else{
+			n.setPrev(tail);
 			tail.next=n;
 			tail=n;
 		}
@@ -76,129 +93,120 @@ public class SLL {
 		return this.size;
 	}
 
-	void printSLL(){
+	StringBuilder printSLL(){
+		StringBuilder buff = new StringBuilder();
 		node temp=this.head;
 		while(temp!=null){
-			System.out.print(temp.getValue()+" ");
+			buff.append(temp.getValue()+" ");
 			temp=temp.getNext();
 		}
-		System.out.println();
+		buff.append('\n');
+		return buff;
 	}
 
-	//iterative reverse
-
-	void reverse(){
-		if(head==null)return;
-		node current=head;
-		node next=head.getNext();
-		current.setNext(null);
-		while(next!=null){
-			node temp=next.getNext();
-			next.setNext(current);
-			current=next;
-			next=temp;
-		}
-		tail=head;
-		head=current;
-	}
-	/*
-	 *  DeleteFriend=false
-    for i = 1 to Friend.length-1
-         if (Friend[i].popularity < Friend[i+1].popularity)
-            delete i th friend
-            DeleteFriend=true
-            break
-    if(DeleteFriend == false)
-        delete the last friend
-	 */
 	void deleteFriend(){
 		if(head==null)return;
 		if(head==tail){
-//			System.out.println("inside this head==tail");
 			head=tail=null;
 			return;
 		}
 		node del=null;
 		node prev=null;
-	for(node temp=head;temp.next!=null;){
-		if(temp.getValue() < temp.getNext().getValue()){
-//			System.out.println("inside temp.getValue() < temp.getNext().getValue");
-//			System.out.println("del"+temp.getValue());
-			del=temp;
-			break;
+		for(node temp=head;temp.next!=null;){
+			
+			if(temp.getValue() < temp.getNext().getValue()){
+				del=temp;
+				break;
+			}
+			prev=temp;
+			temp=temp.next;
 		}
-		prev=temp;
-		temp=temp.next;
-	}
-	
-	if(del==head){
-//		System.out.println("inside this del==head");
-		head=head.next;
-		return;
-	}
-	else if(del==tail || del==null){
-	//	    System.out.println("inside this del==tail");
+
+		if(del==head){
+			head=head.next;
+			return;
+		}
+		else if(del==tail || del==null){
 			prev.next=null;
 			tail=prev;
 			return;
 		}
-	else if(del!=null){
-//		System.out.println("inside del!=null");
-//		System.out.println("prev val"+prev.getValue());
-//		System.out.println("del val"+del.getValue());
-		prev.next=del.next;
-	}
-		
-	
-	}
-	
-	
-	
-
-	/*void reverseR(){
-		if(head==null)return;
-		node current=head;
-		node next=head.getNext();
-		current.setNext(null);
-		recursiveReverse(next, current);
-	}*/
-
-	//tail recursive solution
-/*	void recursiveReverse(node next,node current){
-		if(next==null){
-			tail=head;
-			head=current;
-			return;
+		else{
+			prev.next=del.next;
 		}
-		node temp=next.getNext();
-		next.setNext(current);
-		current=next;
-		next=temp;
-		recursiveReverse(next,current);
-	}*/
+		size--;
+	}
+//
+	node deleteFriend(node lastdeleted){
+		if(head==null)return null;
+		if(head==tail){
+			head=tail=null;
+			return null;
+		}
+		node del=null;
+		for(node temp=lastdeleted;temp.next!=null;){
+			
+			if(temp.getValue() < temp.getNext().getValue()){
+				del=temp;
+				break;
+			}
+			temp=temp.next;
+		}
 
-public static void main(String args[]){
+		if(del==head){
+			head=head.next;
+			head.setPrev(null);
+			size--;
+			return head;
+		}
+		else if(del==tail || del==null){
+			tail=tail.prev;
+			tail.next=null;
+			size--;
+			return tail;
+		}
+		else{
+			node prev=del.prev;
+			node next=del.next;
+			prev.setNext(next);
+			next.setPrev(prev);
+			size--;
+			return prev;
+		}
+	}
+
+
+
+	public static void main(String args[]){
 		
+
 		try{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t=Integer.parseInt(br.readLine());
-		while(t-- > 0){
-			SLL sll=new SLL();
-		String nk[]=br.readLine().split(" ");
-		int n=Integer.parseInt(nk[0]);
-		int k=Integer.parseInt(nk[1]);
-		String input[]=br.readLine().split(" ");
-		for(int i=0;i<n;i++){
-			sll.add(Integer.parseInt(input[i]));
-		}
-		for(int i=0;i<k;i++){
-			sll.deleteFriend();
-		}
-		sll.printSLL();
-		sll=null;
-		}
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringBuilder buff = new StringBuilder("");
+			int t=Integer.parseInt(br.readLine());
+
+			while(t-- > 0){
+				SLL sll=new SLL();
+				String nk[]=br.readLine().split(" ");
+				int n=Integer.parseInt(nk[0]);
+				int k=Integer.parseInt(nk[1]);
+				String input[]=br.readLine().split(" ");
+				for(int i=0;i<n;i++){
+					sll.add(Integer.parseInt(input[i]));
+				}
+				node info=sll.head;
+				for(int i=0;i<k;i++){
+					info=sll.deleteFriend(info);
+				}
+				buff.append(sll.printSLL());
+				sll=null;
+			}
+
+			System.out.println(buff);
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 }
+
